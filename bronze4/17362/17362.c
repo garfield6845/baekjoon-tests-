8d400e7fc2c2638331e6fa82c78630575298e885
garfield6845 <yunji10000000@gmail.com>
2024-04-29 23:39:43 +0900

    4-29-2

--- bronze4/17362/17362.c
+++ bronze4/17362/17362.c
@@ -2,8 +2,21 @@
 
 int main(){
     long long int n;
+    int fn;
     scanf("%lld",&n);
-    switch((n/4)%2){
-        case 0:
-    }
+    int n4=n%4;
+    int n42=(n/4)%2;
+    if (n4==3)
+        fn=3;
+    else if(n4==1)
+        if(n42==1)
+            fn=5;
+        else
+            fn=1;
+    else if((n4+n42)%2==0)
+        fn=2;
+    else
+        fn=4;
+
+    printf("%d",fn);
 }
