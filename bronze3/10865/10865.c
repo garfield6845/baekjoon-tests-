c14ccf7d6e336b58f17d64de129e7dac7017b4d3
garfield6845 <yunji10000000@gmail.com>
2024-05-09 18:50:05 +0900

    5-9 first-esy

---
+++ bronze3/10865/10865.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int n,m,a,b;
+    scanf("%d %d",&n,&m);
+    int array1[100000][10000000];//아 메모리 ㄹㅈㄷ로 차지하는거같은데
+    for(int i=0;i<m;i++){
+        scanf("%d %d",&a,&b);
+        //모르겟늠
+    }
+}
